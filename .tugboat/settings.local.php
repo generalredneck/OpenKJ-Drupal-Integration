4233ff4160f5edb39030122805438ac8ab8fd8b5
Allan Chappell <generalredneck@gmail.com>
2018-12-02 14:40:35 -0600

    adding tugboat for testing.

---
+++ .tugboat/settings.local.php
@@ -0,0 +1,11 @@
+<?php
+$databases['default']['default'] = array (
+  'database' => 'tugboat',
+  'username' => 'tugboat',
+  'password' => 'tugboat',
+  'prefix' => '',
+  'host' => 'mysql',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
