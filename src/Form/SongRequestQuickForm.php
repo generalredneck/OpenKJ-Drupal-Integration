ac58066520554edaae1ff6c9b195888e29548d22
Allan Chappell <generalredneck@gmail.com>
2019-01-24 21:18:19 -0600

    Initial work on the Song Request Quick Form.

--- src/Form/SongRequestQuickForm.php
+++ src/Form/SongRequestQuickForm.php
@@ -5,6 +5,9 @@ namespace Drupal\openkj\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\openkj\Entity\Song;
+use Drupal\openkj\Entity\SongRequest;
+use Drupal\openkj\Entity\Venue;
+use Drupal\user\Entity\User;
 
 /**
  * Implements an example form.
@@ -24,6 +27,7 @@ class SongRequestQuickForm extends FormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $song_id = $this->getRequest()->query->get('song_id');
     $song = Song::load($song_id);
+    $venue = Venue::load(1);
     $form['description'] = [
       '#markup' => '<p>' . $this->t(
         'Requesting: %artist - %song',
@@ -54,6 +58,14 @@ class SongRequestQuickForm extends FormBase {
         'group' => $this->t('Group'),
       ],
     ];
+    $form['user_id'] = [
+      '#type' => 'hidden',
+      '#value' => \Drupal::currentUser()->id(),
+    ];
+    $form['venue_id'] = [
+      '#type' => 'hidden',
+      '#value' => $venue->id(),
+    ];
     $form['actions']['#type'] = 'actions';
     $form['actions']['submit'] = [
       '#type' => 'submit',
@@ -68,13 +80,35 @@ class SongRequestQuickForm extends FormBase {
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
+    //TODO: Validate all the ids in the hidden fields.
   }
 
   /**
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-
+    $song_request = new SongRequest();
+    $requester = User::load($form_state->getValue('user_id'));
+    $song = Song::load($form_state->getValue('song_id'));
+    $artist = $song->artist->entity;
+    $venue = Venue::load($form_state->getValue('venue_id'));
+    $song_request->setName($this->t(
+      "Request for @artist - @song @ @venue",
+      [
+        '@artist' => $artist->getName(),
+        '@song' => $song->getName(),
+        '@venue' => $venue->getName(),
+      ]
+    ));
+    $song_request->setOwner($requester);
+    $song_request->set('song', $song);
+    $song_request->set('venue', $venue);
+    $song_request->setPublished(TRUE);
+    $song_request->save();
+    $route_parameters = [
+      'song' => $form_state->getValue('song_id'),
+    ];
+    $form_state->setRedirect('entity.song.canonical', $route_parameters);
   }
 
 }
